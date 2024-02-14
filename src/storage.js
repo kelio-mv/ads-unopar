c92e7bbca8114ca93b6c235fa3f81fac4c8264f2
kelio-mv <keliojosue2@gmail.com>
2024-02-14 14:20:49 -0300

    Updated to 2024.1

--- src/storage.js
+++ src/storage.js
@@ -8,26 +8,21 @@ class Storage {
     const data = JSON.parse(localStorage.getItem(this.appName) || "null");
     if (data) return data;
     else {
-      return schedule.map((d) =>
-        d.readOnly ? d : { ...d, activities: d.activities.map((a) => ({ ...a, done: null })) }
-      );
+      schedule.forEach((discipline) => {
+        if (!discipline.readOnly) {
+          discipline.activities.forEach((activity) => (activity.done = null));
+        }
+      });
+      return schedule;
     }
   }
 
-  editState(di, ai) {
+  editState(disciplineIndex, activityIndex) {
     const nextState = { null: true, true: false, false: null };
-    this.schedule = this.schedule.map((d, i) =>
-      i === di
-        ? {
-            ...d,
-            activities: d.activities.map((a, i) =>
-              i === ai ? { ...a, done: nextState[a.done] } : a
-            ),
-          }
-        : d
-    );
+    const activity = this.schedule[disciplineIndex].activities[activityIndex];
+    activity.done = nextState[activity.done];
     localStorage.setItem(this.appName, JSON.stringify(this.schedule));
-    return this.schedule;
+    return [...this.schedule];
   }
 }
 
